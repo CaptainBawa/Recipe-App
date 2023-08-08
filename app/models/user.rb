d98a57552a67d33471f865f6eac0a3d4cca26f46
Mahabub Alam <mahabubx7@gmail.com>
2023-08-08 23:49:59 +0600

    permissions (cancancan): :hammer: added-authorizations-with-cancancan

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,22 @@
 class User < ApplicationRecord
+  before_validation :set_default
+
   has_many :recipies, dependent: :destroy, class_name: 'Recipe'
   has_many :recipe_foods, dependent: :destroy
   has_many :foods, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable
+
+  validates :name, presence: true
+
+  def set_default
+    self.name = email.split('@')[0]
+  end
+
+  def admin?
+    role == 'admin'
+  end
 end
--- db/migrate/20230808100745_create_users.rb
+++ db/migrate/20230808100745_create_users.rb
@@ -7,7 +7,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.string :encrypted_password, null: false, default: ""
 
       # user informations
-      t.string :name
+      t.string :name, null: false
 
       ## Recoverable
       t.string   :reset_password_token
