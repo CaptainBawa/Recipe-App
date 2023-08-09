f384406a0abbf5d37c3aad9403aa4ceecc938a99
Collins Bawa <collinskwamebawa@yahoo.com>
2023-08-09 21:25:24 +0000

    Fix linters

--- spec/features/recipe_features_spec.rb
+++ spec/features/recipe_features_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'recipies page', type: :feature do
   before(:each) do
-        @user = User.create!(name: 'John', email: 'john@example.com', password: 'password')
-        @recipe = Recipe.create(name: 'test recipe', preparation_time: 1, cooking_time: 1, description: 'testing', user: @user)
-        @food = Food.create(name: 'rice', measurement_unit: 'grams', price: 5.99, quantity: 2, user: @user)
-  
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    @user = User.create!(name: 'John', email: 'john@example.com', password: 'password')
+    @recipe = Recipe.create(name: 'test recipe', preparation_time: 1, cooking_time: 1, description: 'testing', user: @user)
+    @food = Food.create(name: 'rice', measurement_unit: 'grams', price: 5.99, quantity: 2, user: @user)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     visit recipies_path
   end
 
